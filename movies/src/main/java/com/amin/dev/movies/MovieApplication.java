49eea9eb99e8a235681e1b6e9efb0c637c154c40
Amine Oulagha <oulaghaamine@gmail.com>
2022-08-13 12:26:17 +0200

    Application Eureka Server - Activate server registries

--- movies/src/main/java/com/amin/dev/movies/MovieApplication.java
+++ movies/src/main/java/com/amin/dev/movies/MovieApplication.java
@@ -2,12 +2,13 @@ package com.amin.dev.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(
         scanBasePackages = {"com.amin.dev.movies"
         }
 )
-
+@EnableEurekaClient
 public class MovieApplication {
 
     public static void main(String[] args) {
